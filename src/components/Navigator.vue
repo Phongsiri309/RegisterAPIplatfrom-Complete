470efd007a17442de8c8e6809f2dbff0bb5fb568
Phongsiri309 <phongsiri-st@rmutsb.ac.th>
2021-01-21 20:36:36 +0700

    if sign in not sign in

--- src/components/Navigator.vue
+++ src/components/Navigator.vue
@@ -4,13 +4,14 @@
     <b-navbar-brand>API platform</b-navbar-brand>
 
     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
+    
     <b-collapse id="nav-collapse" is-nav>
+     
       <b-navbar-nav>
         <b-nav-item to="/">HOME</b-nav-item>
         <b-nav-item to="/about" >About</b-nav-item>
       </b-navbar-nav>
-
+   
       <!-- Right aligned nav items -->
       <b-navbar-nav class="ml-auto">
         <b-nav-form>
@@ -19,12 +20,11 @@
          </eiei>
           {{ $store.state.user.Name }}
         </b-nav-form>
-
-      
-
         <b-nav-item-dropdown right>
           <!-- Using 'button-content' slot -->
-          <template #button-content>
+          <template #button-content v-if="$store.state.user">
+          </template>
+          <template #button-content v-else >
             <em>SignIn</em>
           </template>
           <b-dropdown-item href="/" >
